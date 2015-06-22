f6b7d7bef33a5dec01d2585667e5504ffdb3881d
Avicov <ale.v_1988@hotmail.com>
2015-06-22 16:19:41 -0300

    se agregan las funciones de alta, baja y modificacion de las subastas

--- Bestnid/model/subasta.model.php
+++ Bestnid/model/subasta.model.php
@@ -50,7 +50,127 @@ class SubastaModel
 			die($e->getMessage());
 		}
 	}
+	
+	public function altaSubasta(Subasta $data) {
+		
+		try 
+		{
+		print_r($data);
+		$sql = "INSERT INTO subasta (usuarioID,categoriaID,nombre,descripcion,imagen,fecha_inicio,fecha_fin) 
+		        VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+		$this->pdo->prepare($sql)
+		     ->execute(
+			array(
+				$data->__GET('usuarioID'), 
+				$data->__GET('categoriaID'), 
+				$data->__GET('nombre'),
+				$data->__GET('descripcion'),
+				$data->__GET('imagen'),
+				$data->__GET('fecha_inicio'),
+				$data->__GET('fecha_fin')
+				)
+			);
+			printf('agregado<br/>');
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}	
+	}
+	
+	public function bajaSubasta($id) {
+		try 
+		{
+			$stm = $this->pdo
+			          ->prepare("DELETE FROM subasta WHERE subastaID = ?");			          
+
+			$stm->execute(array($id));
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}	
+	}
+	
+	public function modificarSubasta(Subasta $data)
+	{
+		try 
+		{
+			$sql = "UPDATE subasta SET 
+						usuarioID		=?,
+						categoriaID		=?,
+						nombre			=?,
+						descripcion		=?,
+						imagen			=?,
+						fecha_inicio	=?,
+						fecha_fin		=?
+						
+				    WHERE subastaID = ?";
+
+			$this->pdo->prepare($sql)
+			     ->execute(
+				array(
+					$data->__GET('usuarioID'), 
+					$data->__GET('categoriaID'), 
+					$data->__GET('nombre'),
+					$data->__GET('descripcion'),
+					$data->__GET('imagen'),
+					$data->__GET('fecha_inicio'),
+					$data->__GET('fecha_fin'),
+					$data->__GET('subastaID')
+					)
+				);
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
 }
 //-------Test Subasta----------
 //$s=new SubastaModel();
 //print_r($s->listar('tele'));
+
+//----------Test Insertar subasta nueva-----------------
+/*require_once('subasta.entidad.php');
+require_once('../db/db.php');
+$subasta=new Subasta();
+
+				$subasta->__SET('usuarioID',1); 
+				$subasta->__SET('categoriaID',1); 
+				$subasta->__SET('nombre','testNombre');
+				$subasta->__SET('descripcion','testDescripcion');
+				$subasta->__SET('imagen','testImg');
+				$subasta->__SET('fecha_inicio','2015-03-19');
+				$subasta->__SET('fecha_fin','2015-04-19');
+				
+$sm=new SubastaModel();
+$sm->altaSubasta($subasta);
+print_r($sm->listar($subasta->__GET('usuarioID')));
+	FUNCIONA OK!!!
+*/
+
+//--------------Test Eliminar subasta ------------------
+/*require_once('subasta.entidad.php');
+require_once('../db/db.php');
+$sm=new SubastaModel();
+$sm->bajaSubasta(6);
+$sm->listar('');
+	FUNCIONA OK!!!
+*/
+
+//--------------Test Modificar subasta------------------
+/*require_once('subasta.entidad.php');
+require_once('../db/db.php');
+$subasta=new Subasta();
+				$subasta->__SET('subastaID',8);
+				$subasta->__SET('usuarioID',1); 
+				$subasta->__SET('categoriaID',1); 
+				$subasta->__SET('nombre','testNombreM');
+				$subasta->__SET('descripcion','testDescripcionM');
+				$subasta->__SET('imagen','testImg');
+				$subasta->__SET('fecha_inicio','2015-03-19');
+				$subasta->__SET('fecha_fin','2015-04-19');
+$sm=new SubastaModel();
+$sm->modificarSubasta($subasta);
+print_r($sm->listar($subasta->__GET('subastaID')));			
+	FUNCIONA OK!!!
+*/
