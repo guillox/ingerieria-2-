9ca093eaa831f3df0683cf3034c4b0c43ae0edc1
jose guillermo <guillox22@gmail.com>
2015-06-27 13:04:22 -0300

    comentariosVisita.php
    Agregado de estilo y entre las preguntas y las respuestas y
    funcionalidades

--- Bestnid/view/comentariosVisita.php
+++ Bestnid/view/comentariosVisita.php
@@ -1,1 +1,43 @@
-<h1> solo comentarios para visitantes</h1>
+
+<h3> listado de comentarios</h3>
+
+
+<?php
+        /*recupera la lista de comentarios ----array*/
+        $listado=$this->model->listar($idSubasta);
+        
+        if(sizeof($listado) == 0)
+        {
+            echo"<p>esta subasta no tiene comentarios</p>";
+        }else{
+            foreach($listado as $r):
+            
+  ?>      
+      
+   			<blockquote>
+  				<p><?php echo $r->__GET('pregunta'); ?>  </p>
+  					<small>Fecha:<cite title="Fecha_pregunta">"<?php echo $r->__GET('fechaPregunta'); ?>"</cite></small>
+				</blockquote>
+            
+                <?php if ($r->__GET('respuesta') != ''){ ?>
+                	<blockquote class="pull-right">
+  						<p><?php echo  $r->__GET('respuesta'); ?></p>
+  						<small>Fecha:<cite title="Fecha_repuesta">"<?php echo $r->__GET('fechaRespuesta'); ?>" </cite></small>
+					</blockquote>
+                <?php } ?>
+                
+					
+
+
+                
+                
+                
+                
+                
+
+                <div id="pregunta">
+                </div> 
+
+<?php       endforeach;
+        } 
+?> 
