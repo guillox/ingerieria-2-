063a88ab2f8d61dc61799d45df4bdfaa8f031f9b
cano92 <canoyoberceo@gmail.com>
2015-07-02 01:36:39 -0300

    version final comentarios
    comentarios completo
    solo remplasenlo en el ultimo proyecto que tenemos y deberia funcionar
    en el index principal -> solo agregue dos lineas para que se carguen las
    animaciones de jquery

---
+++ mod_Comentarios_Final/index.php
@@ -0,0 +1,43 @@
+<?php
+require_once('db/db.php');
+require_once 'controller/usuario.controller.php';
+require_once 'controller/subasta.controller.php';
+require_once 'controller/ofertas.controller.php';
+require_once 'controller/comentarios.controller.php';
+
+
+// Todo esta lógica hara el papel de un FrontController
+if(!isset($_REQUEST['c'])){
+	    
+    $controller = new UsuarioController();
+		if(isset($_SESSION['idUser'])) {    
+    		$controller->vistaLogueado();
+    	}else {
+    		$controller->index();
+    	} 
+} else {
+    
+    // Obtenemos el controlador que queremos cargar
+    $controller = $_REQUEST['c'] . 'Controller';
+    if(isset($_REQUEST['s'])) { 
+    	 $accion     = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'vistaLogueado';
+ 	}else {
+    if(isset($_SESSION['idUser'])) {    
+    		    $accion     = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'vistaLogueado';
+    	}else {
+    		    $accion     = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'index';
+    	} 
+   // $accion     = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'Index';
+    }
+    
+    // Instanciamos el controlador
+    $controller = new $controller();
+    
+    // Llama la accion
+    call_user_func( array( $controller, $accion ) );
+}
+
+?>
+<script type="text/javascript" src="/view/js/jquery-1.11.3.min.js"></script>
+
+<script type="text/javascript" src="view/js/animaciones.js"></script>
